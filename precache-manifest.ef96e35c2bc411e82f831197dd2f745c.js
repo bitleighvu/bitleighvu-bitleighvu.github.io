e35ab772aefd567ee52fee8995115522cb027382
BitLeigh Vu <bitleighvu@gatech.edu>
2020-05-19 17:56:23 -0400

    Updates

--- precache-manifest.ef96e35c2bc411e82f831197dd2f745c.js
+++ precache-manifest.ef96e35c2bc411e82f831197dd2f745c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8f69fe06a7856e919024a106fe50726",
+    "revision": "7a3ea2f045dfe218b0b28270799102dc",
     "url": "/index.html"
   },
   {
-    "revision": "4a89ce6258cc94a94079",
+    "revision": "ab5a967c302f8a1a3d0b",
     "url": "/static/css/2.829c9cb5.chunk.css"
   },
   {
-    "revision": "d77d3ebabda1a38f910b",
+    "revision": "003d74bb2f5b0ce7c6db",
     "url": "/static/css/main.7b9f8ca7.chunk.css"
   },
   {
-    "revision": "4a89ce6258cc94a94079",
-    "url": "/static/js/2.f5f29a59.chunk.js"
+    "revision": "ab5a967c302f8a1a3d0b",
+    "url": "/static/js/2.f81f9a04.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.f5f29a59.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f81f9a04.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d77d3ebabda1a38f910b",
-    "url": "/static/js/main.bfe2c375.chunk.js"
+    "revision": "003d74bb2f5b0ce7c6db",
+    "url": "/static/js/main.7da98213.chunk.js"
   },
   {
     "revision": "5b40f1da272eb39f34e6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d3fdaa9bd1d4818cc2a023b9848c2103.js"
+  "/precache-manifest.ef96e35c2bc411e82f831197dd2f745c.js"
 );
 
 self.addEventListener('message', (event) => {
