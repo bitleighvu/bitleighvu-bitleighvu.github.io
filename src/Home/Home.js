355d40c13761a2f6cee259f540c3a6573d87908e
BitLeigh Vu <bitleighvu@gatech.edu>
2020-05-19 14:05:30 -0400

    add home page

---
+++ src/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+
+  return (  
+    <div>  
+        <img
+        src={process.env.PUBLIC_URL + "/images/macbookHome.jpg"}
+        alt="Facebook Logo"
+        id="large-images"
+        ></img>
+        <div id="name-header">
+            BitLeigh Vu
+        </div>
+        <div id="headliner">
+            4th Year Computer Science Student at Georgia Institute of Technology
+        </div>
+    </div>
+  );
+};
+
+export default Home;
