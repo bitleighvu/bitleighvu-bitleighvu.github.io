d472a7fd9bedcc0efc3d31016d5ac5b6b6be4683
BitLeigh Vu <bitleighvu@gatech.edu>
2020-05-19 16:44:07 -0400

    increase footer col size

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -13,14 +13,14 @@ const Footer = () => {
                 <div id="brand">BitLeigh Vu</div>
                 <SocialMedia />
             </div>
-            <div className="col-4">
-                <div class="row justify-content-around">
-                    <div class="col-4 text-col">
+            <div className="col-5">
+                <div className="row section-header-box">
+                    <div className="col-4 text-col">
                         <a href="/" className="nav-link underline">Home</a>
                         <a href="/about" className="nav-link underline">About</a>
                         <a href="/projects" className="nav-link underline">Projects</a>
                     </div>
-                    <div class="col-4 text-col">
+                    <div class="col-5 text-col">
                         <a href="/resume" className="nav-link underline">Resume</a>
                         <a href="/contact" className="nav-link underline">Contact</a>
                     </div>
