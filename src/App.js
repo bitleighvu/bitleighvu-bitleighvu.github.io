7d30711fa57015f13fffb4d0ab2df67a0edced9a
BitLeigh Vu <bitleighvu@gatech.edu>
2020-05-20 15:53:03 -0400

    resume changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Common/Header';
 import Footer from './Common/Footer';
 import Home from './Home/Home';
 import About from './About/About';
+import Resume from './Resume/Resume';
 import PageNotFound from './PageNotFound';
 
 import './App.css';
@@ -19,6 +20,7 @@ const App = () => {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route path='/about' component={About} />
+            <Route path='/resume' component={Resume} />
             <Route component={PageNotFound} />
           </Switch>
           <Footer />
